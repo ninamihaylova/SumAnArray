b2bf5e6ea907e44cc05f5d38fafac29f474cd58e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-25 16:12:20 +0300

    Initial commit

---
+++ SumAnArray/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int sum = 0;
+        foreach (string number in numbers)
+        {
+            sum += int.Parse(number);
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
+
